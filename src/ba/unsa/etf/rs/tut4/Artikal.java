0fcc54fefec07ac47651b914cbfaf535de4e1c18
Armin <asloboda1@etf.unsa.ba>
2020-03-25 18:10:19 +0100

    Dodali smo prazan konstruktor Artikal.

--- src/ba/unsa/etf/rs/tut4/Artikal.java
+++ src/ba/unsa/etf/rs/tut4/Artikal.java
@@ -5,4 +5,34 @@ public class Artikal {
     private String naziv;
     private double cijena;
 
+
+    public Artikal() {
+
+    }
+
+
+
+    public String getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(String sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
 }
